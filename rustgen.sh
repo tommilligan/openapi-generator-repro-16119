55e7954dc19602ffd67fb0476d9ab1f1f25254e3
Tom Milligan <tom.milligan@uipath.com>
2023-07-18 11:15:26 +0100

    flatted original schema

--- rustgen.sh
+++ rustgen.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-docker run --rm -u "$UID" -v "${PWD}:/local" openapitools/openapi-generator-cli:v7.0.0-beta generate -i /local/schema.json -g rust -o /local/out/rust
+docker run --rm -u "$UID" -v "${PWD}:/local" openapitools/openapi-generator-cli:latest generate -i /local/schema.json -g rust -o /local/out/rust
 
 pushd out/rust
 cargo build
